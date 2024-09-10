5479158d3a826e435203dc62317eff08d97503c8
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-09-10 07:38:47 +0530

    create server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-import express from express;
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import Register from './Register.js';
+import Login from './Login.js';
+
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/register", Register);
+app.use("/login", Login);
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});
