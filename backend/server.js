e425cc02f8617d48fede06e9a931ef9b69392c38
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-09-13 22:41:00 +0530

    changes made to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import registerUser from "./routes/registerRoute.js";
 import login from "./routes/loginRoute.js";
 import connectDB from "./config/db.js";
+// import attendanceRoute from "./routes/attendanceRoute.js";
+import classRoute from "./routes/classRoute.js";
+// import lectureRoute from "./routes/lectureRoute.js";
+import createStudent from "./controller/studentCOntroller.js";
 
 const app = express();
 
@@ -13,6 +17,10 @@ connectDB();
 
 app.post("/register", registerUser);
 app.post("/login", login);
+// app.post("/markAttendance", attendanceRoute);
+app.post("/addClass", classRoute);
+app.post("/createStudent", createStudent);
+// app.post("/addlecture", lectureRoute);
 
 const port = 5001;
 app.listen(port, () => {
