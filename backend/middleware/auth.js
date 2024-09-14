a0de0c2c07e1db13c48dedeee9de01a522c589ad
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-09-14 17:39:22 +0530

    made some changes to middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,26 @@
 import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
-  const AuthHead = req.head("Authorization");
-  const token =
-    AuthHead && AuthHead.startsWith("Bearer ") ? AuthHead.split(" ")[1] : null;
+  const authHeader = req.headers.authorization;
 
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Authorization header missing or malformed" });
+  }
+
+  const token = authHeader.split(" ")[1]; // Extract token from the Authorization header
   if (!token) {
-    return res.status(200).json({ msg: "no token provided" });
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    return res.status(200).json({ msg: "invalid token" });
+    req.username = decoded.username; // Extract username from decoded token
+    console.log("Username from token:", req.username); // Log the username to verify
+    next(); // Proceed to the next middleware or route handler
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
