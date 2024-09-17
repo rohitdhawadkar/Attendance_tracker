6bad1c8a076682ce9c7b594444d9501ddc26768a
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-09-17 23:25:15 +0530

    made some changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,46 @@
 import express from "express";
+import helmet from 'helmet';
+import crypto from 'crypto';
 import cors from "cors";
 import registerUser from "./routes/registerRoute.js";
 import login from "./routes/loginRoute.js";
 import connectDB from "./config/db.js";
 import classRoute from "./routes/classRoute.js";
 import lectureRoute from "./routes/lectureRoute.js";
-import createStudent from "./controller/studentController.js";
+
+import a from "./routes/AttendanceRoute.js";
+
 
 const app = express();
+app.use((req, res, next) => {
+  res.locals.nonce = crypto.randomBytes(16).toString('hex');
+  next();
+});
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      imgSrc: ["'self'", "http://localhost:3001"],
+      fontSrc: ["'self'", "data:"],
+      styleSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
+    },
+  })
+);
 
 app.use(cors());
 app.use(express.json());
 
-connectDB();
 
+connectDB();
+app.use("/attendance", a);
 app.post("/register", registerUser);
 app.post("/login", login);
 app.post("/addClass", classRoute);
-app.post("/createStudent", createStudent);
+
 
 app.use("/lectures", lectureRoute);
 
-const port = 5001;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
