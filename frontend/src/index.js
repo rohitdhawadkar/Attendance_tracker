6d03cc54083089a69a5382476bd0d1b9b36daf94
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-10-09 00:17:19 +0530

    changes made

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Dashboard from "./Dashboard.js"; // Ensure Dashboard is properly defined
+import AttendanceDashboard from "./AttendanceDashboard"; // Ensure Dashboard is properly defined
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CreateAccount from "./createAcoount";
+import CreateAccount from "./createAccount";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<AttendanceDashboard />} />
       <Route path="/signup" element={<CreateAccount />} />
     </Routes>
   </Router>,
